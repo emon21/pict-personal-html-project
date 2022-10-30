077a42a102422a5d2ddc04ad8559d73ae4d352b6
emon21 <emonbd66@gmail.com>
2022-10-30 18:24:40 +0600

    fix:mobile responsive complate

--- public/main.js
+++ public/main.js
@@ -95,6 +95,7 @@ $('.testimonial').slick({
 
      //menu responsive
      function Menu(e) {
+        console.log('alert');
         let list = document.querySelector('ul');
         e.name === 'menu' ? (e.name = "close", list.classList.add('top-[47px]'),
          list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[47px]'), list.classList.remove('opacity-100'))
